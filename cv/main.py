d4fb100189644b060c30a043be0bb2c21ec29074
mzums <annsie035@gmail.com>
2025-04-18 13:27:01 +0200

    detect central cell

--- cv/main.py
+++ cv/main.py
@@ -1,53 +1,120 @@
-import cv2 
-import numpy as np 
+import cv2
+import numpy as np
 
 
-def detect_grid_lines(edges, image, draw):
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=10, minLineLength=100, maxLineGap=20)
-
-    horizontal = []
-    vertical = []
-    img_with_lines = image.copy()
-
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
-            
-            if abs(angle) < 25:
-                color = (0, 0, 255)
-                horizontal.append((y1 + y2) / 2)
-            elif abs(angle - 90) < 25:
-                color = (0, 255, 0)
-                vertical.append((x1 + x2) / 2)
-            else:
-                continue
-            
-            cv2.line(img_with_lines, (x1, y1), (x2, y2), color, 10) if draw else None
-        
-        if draw:
-            cv2.imshow('Line detection', img_with_lines)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-    else:
-        print("Lines not found")
-
-    return lines
+def show_image(title, image):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
 
 
-if __name__ == '__main__':
-    image = cv2.imread('unnamed.png')
+def preprocess(image_path, draw = False):
+    image = cv2.imread(image_path)
     if image is None:
-        print("Error: couldn't load the image")
-        exit()
-
+        print("Error: Couldn't load the image")
+        return
+    
+    show_image("1. Original", image) if draw else None
+    
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    edges = cv2.Canny(gray, 5, 50)
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    show_image('2. Thresholded Image', thresh) if draw else None
 
-    cv2.imshow('Line detection', edges)
+    edges = cv2.Canny(thresh, 5, 50)
+    show_image('3. Edge Detection', edges) if draw else None
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    closed_edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    show_image('4. Closed Edges', closed_edges) if draw else None
+
+    filled = np.zeros_like(gray)
+    contours, _ = cv2.findContours(closed_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(filled, contours, -1, 255, thickness=cv2.FILLED)
+    show_image('5. Filled Contours', filled) if draw else None
+
+    return image, closed_edges, contours, filled
+
+
+def detect_grid_center(image, closed_edges, contours, filled, draw=False):
+    center_point = (image.shape[1]//2, image.shape[0]//2)
+    best_contour = None
+    min_distance = float('inf')
+
+    all_contours_img = image.copy()
+    cv2.drawContours(all_contours_img, contours, -1, (0,255,0), 2)
+    show_image('6. All Contours', all_contours_img) if draw else None
+    cv2.waitKey(0)
+
+    for cnt in contours:
+        M = cv2.moments(cnt)
+        if M["m00"] == 0:
+            continue
+            
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        
+        distance = np.sqrt((cx - center_point[0])**2 + (cy - center_point[1])**2)
+        
+        area = cv2.contourArea(cnt)
+        x,y,w,h = cv2.boundingRect(cnt)
+        aspect_ratio = float(w)/h
+        
+        if area > 2000 and 0.7 < aspect_ratio < 1.3 and distance < min_distance:
+            min_distance = distance
+            best_contour = cnt
+
+    result = image.copy()
+    if best_contour is not None:
+        M = cv2.moments(best_contour)
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        
+        cv2.drawContours(result, [best_contour], -1, (0,255,0), 3)
+        cv2.circle(result, (cx, cy), 10, (0,0,255), -1)
+        print(f"Center of middle cell: ({cx}, {cy})")
+    else:
+        print("Middle cell not found")
+
+    get_center_size(cx, cy, filled)
+
+    show_image('7. Final Result', result) if draw else None
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    detect_grid_lines(edges, image, True)
 
+def get_center_size(cx, cy, filled):
+    h, w = filled.shape
+
+    left = cx
+    while left > 0 and filled[cy, left] == 255:
+        left -= 1
+    left += 7
+    
+    right = cx
+    while right < w-1 and filled[cy, right] == 255:
+        right += 1
+    right -= 7
+    
+    top = cy
+    while top > 0 and filled[top, cx] == 255:
+        top -= 1
+    top += 7
+    
+    bottom = cy
+    while bottom < h-1 and filled[bottom, cx] == 255:
+        bottom += 1
+    bottom -= 7
+    
+    result = cv2.cvtColor(filled, cv2.COLOR_GRAY2BGR)
+    
+    cv2.line(result, (left, cy), (right, cy), (0,0,255), 1)
+    cv2.line(result, (cx, top), (cx, bottom), (0,0,255), 1)
+    
+    cv2.rectangle(result, (left, top), (right, bottom), (0,0,255), 2)
+
+    show_image("title", result)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    image, closed_edges, contours, filled = preprocess('image3.jpg')
+    detect_grid_center(image, closed_edges, contours, filled)
